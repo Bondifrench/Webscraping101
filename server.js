5b4cf5d67c162851e09be42e95ad0ccb5048e48c
Bondifrench <d_dumaine@yahoo.com>
2014-05-28 17:58:15 +1000

    webscraping with json output

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ var request = require('request');
 var cheerio = require('cheerio');
 
 app.get('/scrape', function (req, res) {
-	url='http://www.imdb.com/title/tt0111161/';
+	url='http://www.asx.com.au/asx/research/companyInfo.do?by=asxCode&asxCode=AGK';
 	request(url, function (error, response, html) {
 		//First we make sure there are no errors when making the request
 		if(!error) {
@@ -13,33 +13,31 @@ app.get('/scrape', function (req, res) {
 			var parsedResults = [];
 			//Finally we define the variables we're going to capture
 			//We use the unique header class as a starting point
-			$('.header').filter(function () {
+			$('#company-information').filter(function () {
 				// Let's store the data we filter into a variable  so we can see what's going on
 				var data = $(this);
 				//When examining the DOM we notice that the title rests within the first child element of the header tag.
 				//Using jQuery we can navigate and get the text by writing the following code
-				title = data.children().first().text();
-				release=data.children().last().children().text();
-				
+				name = data.children().first().text();
+				//console.log(name);
 				});
-			$('.star-box.giga-star').filter(function () {
-				var data = $(this);
-				rating = data.children().first().text();
-				
+			var companyDetails={};
+			$('.contenttable.company-details tr').each(function (i, el) {
+				var h = $('th', this).text(), d = $('td', this).text().trim();
+				companyDetails[h]=d;
 			});
-			var MovieInfo = {
-					title: title,
-					release: release,
-					rating: rating
+			console.log(companyDetails);
+			//console.log(details);
+			var companyInfo = {
+					name: name,
+					companyDetails: companyDetails
 				};
-			parsedResults.push(MovieInfo);
-			console.log(parsedResults);
-
+			parsedResults.push(companyInfo);
+			//console.log(parsedResults);
 		}
 		res.set('Content-type', 'text/html');
 		res.send(parsedResults);
 	})
-
 })
 app.listen('8081')
 console.log('Magic happens on port 8081');
